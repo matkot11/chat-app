2041dd977e3512a7d64953f62e5db0c3c3f50c2e
Mateusz Kocik <matkot11@gmail.com>
2025-01-15 17:45:12 +0100

    feature: Create main page

--- src/features/home/HomeView.tsx
+++ src/features/home/HomeView.tsx
@@ -1,9 +1,32 @@
-function HomeView() {
+import styles from './HomeView.module.scss';
+import { Separator } from '@/components/ui/separator.tsx';
+import { buttonVariants } from '@/components/ui/button.tsx';
+import { Github } from 'lucide-react';
+import PageCards from '@/features/home/components/PageCards.tsx';
+
+export default function HomeView() {
   return (
-    <>
-      <img src="/title.png" alt="Title" />
-    </>
+    <div className={styles.wrapper}>
+      <img src="/title.png" alt="Title" className={styles.image} />
+      <p className={styles.text}>
+        Hello fellow friend, welcome in universe, where you can chat with anyone
+        from Rick and Morty universe.
+      </p>
+
+      <Separator />
+
+      <h3 className={styles.header}>Jump to:</h3>
+      <PageCards />
+
+      <Separator />
+
+      <h3 className={styles.header}>Check out source code:</h3>
+      <a
+        className={buttonVariants({ className: styles.github })}
+        href="https://github.com/matkot11/chat-app"
+      >
+        <Github /> Github
+      </a>
+    </div>
   );
 }
-
-export default HomeView;
